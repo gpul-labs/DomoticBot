7e940499f1d8cf93673bc743d0884b300c93d39c
nixijav <nixijav@nixijav.com>
2016-04-09 15:57:26 +0200

    Added init to bmp180 and pir

--- src/main/nodebase/bmp180.py
+++ src/main/nodebase/bmp180.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 import Adafruit_BMP.BMP085 as BMP085
-sensor = BMP085.BMP085()
 
-def temp():
-   """
-   Returns temperature in C*
-   """
-   return '{0:0.2f}'.format(sensor.read_temperature())
-def pressure():
-   """
-   Returns atmosferic pressure in pascals. 1 atm = 101325 Pa
-   """
-   return '{0:0.2f}'.format(sensor.read_pressure())
-def altitude():
-   """
-   Returns altitude in meters relative to sea level. Temp dependent
-   """
-   return '{0:0.2f}'.format(sensor.read_altitude())
+class bmp180():
+   def temp(self):
+      """
+      Returns temperature in C*
+      """
+      return float('{0:0.2f}'.format(self.sensor.read_temperature()))
+   def pressure(self):
+      """
+      Returns atmosferic pressure in pascals. 1 atm = 101325 Pa
+      """
+      return float('{0:0.2f}'.format(self.sensor.read_pressure()))
+   def altitude(self):
+      """
+      Returns altitude in meters relative to sea level. Temp dependent
+      """
+      return float('{0:0.2f}'.format(self.sensor.read_altitude()))
+
+   def __init__(self):
+      self.sensor = BMP085.BMP085()
--- src/main/nodebase/pir.py
+++ src/main/nodebase/pir.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+class pir():
+   def isPirActive(self):
+      return True if GPIO.input(23) else False
 
-def isPirActive():
-   return True if GPIO.input(23) else False
+   def __init__(self):
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
