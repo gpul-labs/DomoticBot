08bfdc4bc42058e046f92b2b38d1dcc21501eb6a
Pedro Vale Ramos <pedro.vale.ramos@gmail.com>
2016-04-09 16:37:45 +0200

    Creates NodeBase classes (only structure)

---
+++ src/main/nodebase/NodeBase.py
@@ -0,0 +1,24 @@
+import bmp180
+
+class NodeBase:
+    def __init__(self):
+
+
+class NodePresence:
+    def __init__(self):
+
+
+class NodeDoorbell:
+    def __init__(self):
+
+
+class NodeClimatization:
+    def __init__(self):
+        self.bmp180 = bmp180()
+        #self.hotterActuator = switch()
+        #self.coolerActuator = switch()
+
+
+class NodeDoorlock:
+    def __init__(self):
+
