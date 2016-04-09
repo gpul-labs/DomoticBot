8c145ce83ff272736770e97ff006ab338f4ff711
= <elmadno@gmail.com>
2016-04-09 16:46:00 +0200

    add Notify class to notify temperature each delay

---
+++ src/main/commands/Notify.py
@@ -0,0 +1,37 @@
+import imp
+import pprint
+import thread
+import time
+import telepot
+
+# Help class to show the help in Telegram Bot
+class Notify:
+    def __init__(self):
+        self.node = imp.load_source('NodeBase', '../../nodebase/NodeBase.py')
+        self.enabled = false
+        self.delay = 1 * 60
+
+    # Notifiy after period
+    def processMessage(self, message):
+        content_type, chat_type, chat_id = telepot.glance(message)
+        command = message['text'].strip().lower()
+        text = command[0]
+        enabled =  command[1]
+        delay = command[2]
+
+        if enabled != 'on' or enabled != 'off':
+            return "Error: I do not understand"
+
+        if delay.isdigit():
+            return "Error: minutes are not numeric"
+
+        if content_type == 'text' and text == '/notify':
+            self.enabled = enabled
+            self.delay = delay * 60
+            if enabled:
+                thread.start_new_thread ( self.notify )
+
+    def notify():
+        while 1:
+            return self.node.getTemp()
+            time.sleep(self.delay)
